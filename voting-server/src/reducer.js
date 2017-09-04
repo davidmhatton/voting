73aab5bf32d4f697481461376438c12fd9778c79
David Hatton <dhatton@turnitin.com>
2017-09-04 23:50:23 +0100

    reducer logic

--- voting-server/src/reducer.js
+++ voting-server/src/reducer.js
@@ -1,3 +1,16 @@
-export default function reducer(state, action) {
-  
+import { setEntries, next, vote, INITIAL_STATE } from './core';
+
+export default function reducer(state = INITIAL_STATE, action) {
+  switch (action.type) {
+  case 'SET_ENTRIES':
+    setEntries(state, action.entries);
+    break;
+  case 'NEXT':
+    next(state);
+    break;
+  case 'VOTE':
+    vote(state, action.entry);
+    break;
+  }
+  return state;
 }
