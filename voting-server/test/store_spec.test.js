28bfd6a53cfafa73ef1982cdcce0bc6d7779dc31
David Hatton <dhatton@turnitin.com>
2017-09-30 17:45:14 +0100

    set up store

---
+++ voting-server/test/store_spec.test.js
@@ -0,0 +1,20 @@
+import {Map, fromJs} from 'immutable';
+import {expect} from 'chai';
+
+import makeStore from '../src/store';
+
+describe('store', () => {
+
+  it('is a redux store configured with the correct reducer', () => {
+    const store = makeStore;
+    expect(store.getState()).to.equal(Map());
+
+    store.dispatch({
+      type: 'SET_ENTRIES',
+      entries: ['Trainspotting', '28 Days Later']
+    });
+    expect(store.getState()).to.equal(fromJs({
+      entries: ['Trainspotting', '28 Days Later']
+    }));
+  });
+});
